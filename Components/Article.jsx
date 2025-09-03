f9e83723b74cd1ba6b6f05285e4b74bcc4519a24
Arimateia <sousaarimateia4567@gmail.com>
2025-09-03 14:24:54 -0300

    Criação componente noticias

---
+++ Components/Article.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { TbClockHour4 } from "react-icons/tb";
+
+export const Article = ({
+  author,
+  title,
+  description,
+  url,
+  urlToImage,
+  sourceName,
+  publishedAt,
+  isMain = false,
+  className,
+}) => {
+  const formatTime = (isoDate) => {
+    const date = new Date(isoDate);
+    return date.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  return isMain ? (
+    <div
+      className={`relative flex ${className}`}
+    >
+      {urlToImage && (
+        <img
+          src={urlToImage}
+          alt={title}
+          className="w-[50rem] h-[35rem] object-cover rounded-lg"
+        />
+      )}
+
+      {/* Gradiente sobre a imagem */}
+      <div className="absolute inset-0 rounded-lg bg-gradient-to-t from-black/80 via-black/30 to-transparent z-10"></div>
+
+      {/* Informações */}
+      <div className="absolute bottom-5 left-5 z-20 text-white p-3 rounded-md w-4/5">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <div className="flex items-center gap-4 mb-2">
+            <h3 className="text-sm">{author}</h3>
+            <small className="flex items-center gap-1 text-xs">
+              <TbClockHour4 /> {formatTime(publishedAt)}
+            </small>
+          </div>
+          <span className="font-bold text-[23px] leading-snug">{title}</span>
+        </a>
+        {description && <p className="mt-2 text-sm">{description}</p>}
+      </div>
+    </div>
+  ) : (
+    <div
+      className={`flex items-center mb-4 border-b border-gray-300 pb-3 ${className}`}
+    >
+      {urlToImage && (
+        <img
+          src={urlToImage}
+          alt={title}
+          className="max-w-[200px] max-h-[200px] w-auto h-auto rounded-lg mr-3"
+        />
+      )}
+      <div className="flex flex-col">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <div className="flex items-center gap-4 mb-1">
+            <h3 className="text-sm">{author}</h3>
+            <small className="flex items-center gap-1 text-xs">
+              <TbClockHour4 /> {formatTime(publishedAt)}
+            </small>
+          </div>
+          <p className="font-bold text-lg leading-snug">{title}</p>
+        </a>
+        <small className="text-gray-500">{sourceName}</small>
+      </div>
+    </div>
+  );
+};
---
+++ Components/LatestNews.jsx
@@ -0,0 +1,73 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Article } from "./Article";
+
+export const LatestNews = () => {
+  const [articles, setArticles] = useState([]);
+
+  // api
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=brasil&language=pt&pageSize=10&apiKey=d5bbcab384a44b4a9a3c0212c6c692f4`
+      );
+
+      const { articles: fetchedArticles } = response.data;
+
+      const sortedArticles = fetchedArticles.sort(
+        (a, b) =>
+          new Date(b.publishedAt).getTime() -
+          new Date(a.publishedAt).getTime()
+      );
+
+      setArticles(sortedArticles);
+
+      console.log(sortedArticles);
+    } catch (error) {
+      console.error("Erro ao buscar noticias:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  if (articles.length === 0) {
+    return <p>Carregando notícias...</p>;
+  }
+
+  const [mainArticle, ...sideArticles] = articles;
+  return (
+    <div className="flex gap-4 my-8 mx-28 m">
+      {/* artigo em destaque */}
+      <div className="flex-1">
+        <Article
+          title={mainArticle.title}
+          author={mainArticle.author}
+          description={mainArticle.description}
+          url={mainArticle.url}
+          urlToImage={mainArticle.urlToImage}
+          sourceName={mainArticle.source.name}
+          publishedAt={mainArticle.publishedAt}
+          isMain={true}
+        />
+      </div>
+
+      {/* próximos 4 artigos */}
+      <div className="flex flex-col gap-4 w-[350px] ">
+        {sideArticles.slice(0, 4).map((article, index) => (
+          <Article
+            key={index}
+            author={article.author}
+            title={article.title}
+            url={article.url}
+            sourceName={article.source.name}
+            publishedAt={article.publishedAt}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};

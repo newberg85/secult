1cddd4dfa70e5d10bdd457dbf8449243e3302744
newberg85 <bergviana12345@gmail.com>
2025-09-03 19:05:19 -0300

    adicionando axios

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
 import Slide from "@/Components/Slide";
 
 
 export default function Home() {
   return (
-    <div className="font-sans grid-rows-[20px_1fr_20px] items-center min-h-screen pb-20 gap-16">
+    <div className="font-sans grid-rows-[20px_1fr_20px] items-center min-h-screen gap-16">
       <Header />
       <Slide />
+      <Footer />
     </div>
   );
 }

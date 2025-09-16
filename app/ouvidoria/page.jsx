0c12bfe94fb38b72ea025201a8e86cf554062d68
Arimateia <sousaarimateia4567@gmail.com>
2025-09-16 13:59:26 -0300

    Alterações na barra de pesquisa

--- app/ouvidoria/page.jsx
+++ app/ouvidoria/page.jsx
@@ -1,15 +1,16 @@
 import Header from "@/Components/Header";
+import Slide from "@/Components/Slide";
+import Image from "next/image";
+import Link from "next/link";
 import Footer from "@/Components/Footer";
 import Ouvidoria from "@/Components/Ouvidoria";
- 
 
 export default function Home() {
-
   return (
     <div className=" grid-rows-[20px_1fr_20px] items-center min-h-screen gap-16 font[Montserrat]">
-      <Header />
+      <Header/>
       <Ouvidoria />
       <Footer />
     </div>
-  );
+    );
 }

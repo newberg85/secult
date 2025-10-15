f990491347a535191ebb10791c85b0882a7197e4
newberg85 <bergviana12345@gmail.com>
2025-10-15 11:52:35 -0300

    configurações BD

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,7 +1,19 @@
-export default function Layout({children}) {
-    return (
-        <> 
+import Sidebar from "@/Components/AdminComponents/Sidebar";
+import { FaUser } from "react-icons/fa";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex flex-col w-full">
+            <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+                <h3 className="font-medium">Painel do Administrador</h3>
+                <FaUser className="font-3xl"/>
+            </div>
             {children}
-        </>
-    )
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ app/api/noticia/route.js
@@ -0,0 +1,27 @@
+import { ConnectDB } from "@/lib/config/db";
+const { NextResponse } = require("next/server");
+import { writeFile } from 'fs/promises'
+
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
+LoadDB();
+
+export async function GET(request) {
+  return NextResponse.json({ msg: "API Funcionando" });
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`
+  console.log(imgUrl)
+  return NextResponse.json({imgUrl})
+}

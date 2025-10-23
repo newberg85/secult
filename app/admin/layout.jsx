a3f949ebe5885b7269af1c1dd12244fc0011bc20
Arimateia <sousaarimateia4567@gmail.com>
2025-10-23 15:06:57 -0300

    Alterações no login

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -4,14 +4,12 @@ import ProtectedRoute from '../components/ProtectedRoute';
 
 const AdminLayout = ({ children }) => {
   return (
-    <ProtectedRoute>
       <div className="flex">
         <AdminNav />
         <main className="flex-1 p-6 bg-gray-50 min-h-screen">
           {children}
         </main>
       </div>
-    </ProtectedRoute>
   );
 };
 
